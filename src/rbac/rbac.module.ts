7d4b8a9655fdf36d816ed52021c0861c1adf821c
Khalid Al-Mutawa <khalid@bawes.net>
2024-12-16 03:28:12 +0300

    Cleanup unused structure of permissions

--- src/rbac/rbac.module.ts
+++ src/rbac/rbac.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { DiscoveryModule } from '@nestjs/core';
 import { PermissionDiscoveryService } from './services/permission-discovery.service';
+import { PermissionManagementService } from './services/permission-management.service';
 import { PermissionGuard } from './guards/permission.guard';
 import { PrismaModule } from '../prisma/prisma.module';
 import { CacheModule } from '@nestjs/cache-manager';
+import { PermissionManagementController } from './controllers/permission-management.controller';
+import { RoleManagementController } from './controllers/role-management.controller';
 
 @Module({
   imports: [
@@ -13,7 +16,8 @@ import { CacheModule } from '@nestjs/cache-manager';
       ttl: 300, // 5 minutes
     }),
   ],
-  providers: [PermissionDiscoveryService, PermissionGuard],
-  exports: [PermissionDiscoveryService, PermissionGuard],
+  controllers: [PermissionManagementController, RoleManagementController],
+  providers: [PermissionDiscoveryService, PermissionManagementService, PermissionGuard],
+  exports: [PermissionDiscoveryService, PermissionManagementService, PermissionGuard],
 })
 export class RbacModule {}
