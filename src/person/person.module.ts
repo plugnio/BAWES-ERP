5aa5c4e4b96403920d4113b0ab90848330d7e2d8
Khalid Al-Mutawa <khalid@bawes.net>
2024-12-18 16:25:20 +0300

    Fix person rbac

--- src/person/person.module.ts
+++ src/person/person.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PersonService } from './person.service';
 import { PersonController } from './person.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { RbacModule } from '../rbac/rbac.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, RbacModule],
   controllers: [PersonController],
   providers: [PersonService],
   exports: [PersonService],
